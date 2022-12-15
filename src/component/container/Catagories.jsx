a8e175994cb8e0c2e796ec89faf3f45008879914
MRshahed <shahedhossensaad@gmail.com>
2022-12-16 00:05:49 +0600

    added products

--- src/component/container/Catagories.jsx
+++ src/component/container/Catagories.jsx
@@ -13,21 +13,21 @@ const Catagories = () => {
         centeredSlides={false}
         spaceBetween={20}
         grabCursor={true}
-        // breakpoints={{
-        //   640: {
-        //     slidesPerView: 1,
-        //   },
-        //   768: {
-        //     slidesPerView: 2,
-        //   },
-        //   1024: {
-        //     slidesPerView: 3,
-        //   },
-        // }}
+        breakpoints={{
+          200: {
+            slidesPerView: 1,
+          },
+          768: {
+            slidesPerView: 2,
+          },
+          1024: {
+            slidesPerView: 4,
+          },
+        }}
         className="mySwiper h-[70vh] w-[100%]"
       >
         <SwiperSlide className="">
-          <div className="relative bg-purple-900 h-full w-full">
+          <div className="relative bg-purple-900 h-full w-full ">
             <img src={img1} alt="img" className="h-full w-full object-cover " />
             <div className="flex flex-col absolute top-0 left-0 h-full w-full bg-gray-800 bg-opacity-20 justify-center items-center gap-y-6">
               <h1 className="text-white font-bold text-3xl">
--- src/component/container/Product.jsx
+++ src/component/container/Product.jsx
@@ -6,7 +6,7 @@ import { popularProducts } from "../../component/Data";
 
 const Product = () => {
   return (
-    <div className="grid grid-cols-4 gap-5 items-center justify-center h-[70vh] w-full px-10 ">
+    <div className="grid lg:grid-cols-4 gap-5 items-center justify-center h-[70vh] w-full px-10 md:grid-cols-2 sm:grid-cols-1">
       {popularProducts.map((product) => (
         <div className=" h-[400px] w-[350px] bg-purple-100 flex items-center justify-center relative transition cursor-pointer ease-in-out hover:scale-105 hover:z-10">
           <img

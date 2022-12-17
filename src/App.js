88b066b1c0144ba527d08a8c74786bdc69930ed7
MRshahed <shahedhossensaad@gmail.com>
2022-12-18 01:41:56 +0600

    added products

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./component/pages/Home";
+import ProductList from "./component/pages/ProductList";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="products" element={<ProductList />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/component/pages/ProductList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../navbar/Navbar";
+import Announcement from "../container/Announcement";
+
+const ProductList = () => {
+  return (
+    <div>
+      <Navbar />
+      <Announcement />
+    </div>
+  );
+};
+
+export default ProductList;

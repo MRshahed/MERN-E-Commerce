0eb0bb9c3524541843a49f7c29ab4bdd3de30e9b
MRshahed <shahedhossensaad@gmail.com>
2022-12-24 00:03:58 +0600

    added models

--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ const mongoose = require("mongoose");
 const app = express();
 app.use(express.json());
 
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.MONGO_URL);
+
 app.listen("5000", () => {
   console.log("Server is running on Port 5000");
 });
---
+++ server/routes/Users.js
@@ -0,0 +1,6 @@
+const Router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+module.exports = Router;

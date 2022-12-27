77e5e46e968995e7c67ee72bbeb7ba5e1f117026
MRshahed <shahedhossensaad@gmail.com>
2022-12-27 23:36:04 +0600

    added wishlist  model

---
+++ server/models/WishList.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const WishListSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    products: [
+      {
+        productId: {
+          type: String,
+        },
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("WIshList", WishListSchema);

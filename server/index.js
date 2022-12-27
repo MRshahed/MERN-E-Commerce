cf793182d3ee0b2055e9d590508d2b22530ff9ac
MRshahed <shahedhossensaad@gmail.com>
2022-12-27 23:21:41 +0600

    added user model

--- server/index.js
+++ server/index.js
@@ -8,10 +8,6 @@ app.use(express.json());
 mongoose.set("strictQuery", false);
 mongoose.connect(process.env.MONGO_URL);
 
-app.use("/", (req, res) => {
-  console.log("hello world");
-});
-
 app.listen("5000", () => {
   console.log("Server is running on Port 5000");
 });

e96f490838d841a760d1c71ed1ee3b9fb413f470
MRshahed <shahedhossensaad@gmail.com>
2022-12-28 00:17:02 +0600

    added auth

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-
+const authRoute = require("./routes/Auth");
 const app = express();
 app.use(express.json());
 
 mongoose.set("strictQuery", false);
 mongoose.connect(process.env.MONGO_URL);
+
+app.use("/auth", authRoute);
 
 app.listen("5000", () => {
   console.log("Server is running on Port 5000");

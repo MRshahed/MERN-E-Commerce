c438b310fe9640ef8389900c44a60f101f119132
MRshahed <shahedhossensaad@gmail.com>
2022-12-27 23:27:57 +0600

    added product  model

---
+++ server/models/Product.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      required: true,
+    },
+    catagories: {
+      type: Array,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    color: {
+      type: String,
+    },
+    size: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", ProductSchema);

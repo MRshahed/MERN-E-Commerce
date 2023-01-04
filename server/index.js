204a2291fea07a428f8f883f0b011a9812a3985a
MRshahed <shahedhossensaad@gmail.com>
2023-01-04 23:02:13 +0600

    added product  route

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/Auth");
 const userRoute = require("./routes/Users");
+const productRoute = require("./routes/Products");
 const app = express();
 app.use(express.json());
 
@@ -11,6 +12,7 @@ mongoose.connect(process.env.MONGO_URL);
 
 app.use("/auth", authRoute);
 app.use("/user", userRoute);
+app.use("/product", productRoute);
 
 app.listen("5000", () => {
   console.log("Server is running on Port 5000");
---
+++ server/routes/Products.js
@@ -0,0 +1,20 @@
+const {
+  VerifyToken,
+  verifyTokenAuth,
+  verifyTokenAdmin,
+} = require("./VerifyToken");
+const Product = require("../models/Product");
+const Router = require("express").Router();
+
+//Create Product
+Router.get("/", verifyTokenAdmin, async (req, res) => {
+  const newProduct = new Product(req.body);
+  try {
+    const product = await newProduct.save();
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = Router;
